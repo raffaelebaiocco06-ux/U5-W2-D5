9e9c392184ab28b68d6e9af59cd54f6b5db87378
raffaelebaiocco06-ux <raffaele.baiocco06@gmail.com>
2026-04-17 09:55:28 +0200

    commit

---
+++ src/test/java/raffaele/U5_w2_d5/U5W2D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package raffaele.U5_w2_d5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W2D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
